ca5dae8d4ec8e26d71a7afdcfad411c6e725f5cd
JjjavierGFX <0550143259javier@gmail.com>
2024-04-24 18:32:55 -0500

    login cambios

--- login/urls.py
+++ login/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.login, name='login'),  # URL para la vista de inicio de sesión
     path('loadministrador/', views.admin_home, name='admin_home'),  # URL para la página de administrador
+    path('edit/<str:cedula>/', views.edit_item, name='edit_item'),  # URL para editar
+    path('delete/<str:cedula>/', views.delete_item, name='delete_item'),  # URL para eliminar
 ]
--- login/views.py
+++ login/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Admin
+from .models import Fisioterapeuta, Paciente
 
 def login(request):
     if request.method == 'POST':
@@ -16,4 +17,44 @@ def login(request):
     return render(request, 'sesion.html')
 
 def admin_home(request):
-    return render(request, 'administrador.html')
+    filter_type = request.GET.get('filter', 'Fisioterapeutas')
+    search_query = request.GET.get('search', '')
+
+    if filter_type == 'Fisioterapeutas':
+        items = Fisioterapeuta.objects.filter(cedula__icontains=search_query)
+    else:
+        items = Paciente.objects.filter(cedula__icontains=search_query)
+
+    context = {
+        'items': items,
+        'filter': filter_type,
+    }
+    return render(request, 'administrador.html', context)
+
+def edit_item(request, cedula):
+    try:
+        fisioterapeuta = Fisioterapeuta.objects.get(cedula=cedula)
+        # Aquí puedes agregar la lógica para editar el fisioterapeuta
+        return render(request, 'editar_fisioterapeuta.html', {'fisioterapeuta': fisioterapeuta})
+    except Fisioterapeuta.DoesNotExist:
+        try:
+            paciente = Paciente.objects.get(cedula=cedula)
+            # Aquí puedes agregar la lógica para editar el paciente
+            return render(request, 'editar_paciente.html', {'paciente': paciente})
+        except Paciente.DoesNotExist:
+            return render(request, 'error.html', {'error': 'El usuario no existe'})
+
+
+def delete_item(request, cedula):
+    try:
+        fisioterapeuta = Fisioterapeuta.objects.get(cedula=cedula)
+        fisioterapeuta.delete()
+        return redirect('admin_home')
+    except Fisioterapeuta.DoesNotExist:
+        try:
+            paciente = Paciente.objects.get(cedula=cedula)
+            paciente.delete()
+            return redirect('admin_home')
+        except Paciente.DoesNotExist:
+            return render(request, 'error.html', {'error': 'El usuario no existe'})
+
