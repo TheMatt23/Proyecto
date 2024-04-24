8c50279613467ba6158a0473da7abf1439169a48
JjjavierGFX <0550143259javier@gmail.com>
2024-04-24 10:55:20 -0500

    login

--- login/views.py
+++ login/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Admin
 
-# Create your views here.
+def login(request):
+    if request.method == 'POST':
+        cedula = request.POST.get('cedula')
+        contrasena = request.POST.get('contrasena')
+        
+        try:
+            admin = Admin.objects.get(cedula=cedula, contrasena=contrasena)
+            return redirect('admin_home')  # Redirige a la página de administrador si las credenciales son correctas
+        except Admin.DoesNotExist:
+            # Aquí puedes agregar un mensaje de error si las credenciales no son correctas
+            return render(request, 'login.html', {'error': 'Cédula o contraseña incorrecta'})
+
+    return render(request, 'login.html')
+
+def admin_home(request):
+    return render(request, 'administrador.html')
