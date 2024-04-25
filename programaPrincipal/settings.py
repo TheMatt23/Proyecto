5c0eeb458925a85744c12dffc8dcb9cda7158115
Mateo Palate <mpalate2620@uta.edu.ec>
2024-04-24 21:46:10 -0500

    a

--- programaPrincipal/settings.py
+++ programaPrincipal/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tdw',
         'USER': 'root',
-        'PASSWORD': 'Javier123!',
+        'PASSWORD': 'mateo2324',
         'HOST' : 'localhost',
         'PORT': ''
     }
