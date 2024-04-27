c9e6e991314e35636e1ed8bc90922c62240f7543
Mateo Palate <mpalate2620@uta.edu.ec>
2024-04-27 02:52:03 -0500

    datos

--- citaMedica/forms.py
+++ citaMedica/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from login.models import CitaMedica,Terapia
+from login.models import Terapia, Movimiento, TipoEjercicio, CitaMedica
 
 class BuscarPacienteForm(forms.Form):
     cedula = forms.CharField(max_length=10, label="Buscar por cédula")
@@ -12,14 +12,33 @@ class CitaMedicaForm(forms.ModelForm):
 class EliminarCitaForm(forms.Form):
     cita_id = forms.IntegerField(widget=forms.HiddenInput()) 
 
-
-class TerapiaForm(forms.Form):
-    cita_id = forms.IntegerField(widget=forms.HiddenInput())
-
 class AgregarTerapiaForm(forms.ModelForm):
    class Meta:
       model = Terapia
       fields = ['nombre'] 
 
-class EliminarTerapiaForm(forms.Form):
-    terapia_id = forms.IntegerField()
+#Terapias
+# forms.py
+class TerapiaForm(forms.ModelForm):
+    class Meta:
+        model = Terapia
+        fields = ['nombre', 'fecha']
+
+# Formulario para agregar movimientos
+class MovimientoForm(forms.ModelForm):
+    class Meta:
+        model = Movimiento
+        fields = ['nombre'] 
+
+# Tipo Ejercico
+class TipoEjercicioForm(forms.ModelForm):
+    class Meta:
+        model = TipoEjercicio
+        fields = ['nombre', 'url']
+
+class EjercicioComboForm(forms.Form):
+    tipo_ejercicio = forms.ModelChoiceField(
+        queryset=TipoEjercicio.objects.all(), 
+        empty_label="Seleccione un tipo",
+        required=True, 
+    )
