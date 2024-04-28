f03b40cb7b6ea664ca0dfa4258697a3a5d1819d3
musill <riky_zapatad@hotmail.com>
2024-04-28 05:09:35 -0500

    Estilos

--- programaPrincipal/settings.py
+++ programaPrincipal/settings.py
@@ -11,11 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+STATIC_DIR = (os.path.join(BASE_DIR,'programaPrincipal/static'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICIFILES_DIR=[STATIC_DIR,]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
