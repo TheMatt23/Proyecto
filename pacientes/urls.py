43081d450a41da9bdba0189c1ba8c6099e61d34e
JjjavierGFX <0550143259javier@gmail.com>
2024-04-28 01:27:02 -0500

    parte pacientes-completado

--- pacientes/urls.py
+++ pacientes/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     # Otras rutas URL aquí...
     path('paciente-detalle/', views.paciente_detalle, name='paciente_detalle'),
+     path('ver-reporte/<int:terapia_id>/', views.ver_reporte, name='ver_reporte'),
+path('reporte-general/', views.reporte_general, name='reporte_general'),
     # Otras rutas URL aquí...
 ]
--- pacientes/views.py
+++ pacientes/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from login.models import Paciente, Terapia
-
+from login.models import Paciente, Terapia, Resultados
+from django.db.models import Sum
+import matplotlib.pyplot as plt
+import plotly.graph_objs as go
+from plotly.offline import plot
 from django.shortcuts import redirect
 
 @login_required
@@ -26,3 +29,94 @@ def paciente_detalle(request):
         # Redirige al usuario a otra vista si no se encuentra la cédula del paciente en la sesión
         return redirect('login')  # Reemplaza 'otra_vista' con el nombre de la vista a la que quieres redirigir al usuario
 
+
+def ver_reporte(request, terapia_id):
+    # Consulta para obtener los valores positivos y negativos de la tabla Resultados
+    resultados = Resultados.objects.filter(movimientoID__terapiaID=terapia_id).aggregate(
+        total_positivo=Sum('cantidadPos'),
+        total_negativo=Sum('cantidadNeg')
+    )
+
+    # Datos del gráfico
+    labels = ['Cantidad Positiva', 'Cantidad Negativa']
+    valores = [resultados['total_positivo'] or 0, resultados['total_negativo'] or 0]
+
+    # Crear el gráfico
+    data = [
+        go.Bar(
+            x=labels,
+            y=valores,
+            marker=dict(color=['rgba(75, 192, 192, 0.2)', 'rgba(255, 99, 132, 0.2)']),
+            opacity=0.8
+        )
+    ]
+
+    layout = go.Layout(
+        title='Reporte Gráfico',
+        xaxis=dict(title='Categorías'),
+        yaxis=dict(title='Cantidad')
+    )
+
+    fig = go.Figure(data=data, layout=layout)
+    plot_div = plot(fig, output_type='div', include_plotlyjs=False)
+
+    return render(request, 'reporteGrafico.html', {'plot_div': plot_div})
+
+def reporte_general(request):
+     # Obtener la cédula del paciente de la sesión
+    cedula_paciente = request.session.get('cedula_paciente')
+
+    if cedula_paciente:
+        # Buscar al paciente por su cédula
+        paciente = Paciente.objects.get(cedula=cedula_paciente)
+
+        # Obtener todas las terapias asociadas al paciente
+        terapias = Terapia.objects.filter(cedulaPaciente=paciente)
+
+        # Inicializar listas para almacenar los resultados de cada terapia
+        resultados_positivos = []
+        resultados_negativos = []
+
+        for terapia in terapias:
+            # Consultar los resultados para cada terapia y sumar los valores positivos y negativos
+            resultados = Resultados.objects.filter(movimientoID__terapiaID=terapia.terapiaID).aggregate(
+                total_positivo=Sum('cantidadPos'),
+                total_negativo=Sum('cantidadNeg')
+            )
+
+            # Agregar los resultados a las listas
+            resultados_positivos.append(resultados['total_positivo'] or 0)
+            resultados_negativos.append(resultados['total_negativo'] or 0)
+
+        # Crear el gráfico
+        data = [
+            go.Bar(
+                x=['Terapia {}'.format(i + 1) for i in range(len(terapias))],
+                y=resultados_positivos,
+                name='Cantidad Positiva',
+                marker=dict(color='rgba(75, 192, 192, 0.2)'),
+                opacity=0.8
+            ),
+            go.Bar(
+                x=['Terapia {}'.format(i + 1) for i in range(len(terapias))],
+                y=resultados_negativos,
+                name='Cantidad Negativa',
+                marker=dict(color='rgba(255, 99, 132, 0.2)'),
+                opacity=0.8
+            )
+        ]
+
+        layout = go.Layout(
+            title='Reporte Gráfico General',
+            xaxis=dict(title='Terapias'),
+            yaxis=dict(title='Cantidad'),
+            barmode='group'
+        )
+
+        fig = go.Figure(data=data, layout=layout)
+        plot_div = plot(fig, output_type='div', include_plotlyjs=False)
+
+        return render(request, 'reporteGeneral.html', {'plot_div': plot_div, 'paciente': paciente})
+    else:
+        # Manejar el caso si no se encuentra la cédula del paciente en la sesión
+        return redirect('login')
